ec9b802ba0dcfe770328e18e4db75d6556c5c9ad
Carlos Souza <(none)>
2016-05-11 08:58:39 -0300

    Utilizando location.js para a página de postagem

--- js/controllers/post.js
+++ js/controllers/post.js
@@ -1,5 +1,7 @@
 Vue.use(VueResource);
 
+var locationHelper = new Location(window.location);
+
 new Vue({
   el: '#content',
   ready: function() {
@@ -7,11 +9,10 @@ new Vue({
       this.$set('posts', response.data);
     });
 
-  	var pattern = /(\w|-)+/;
-		var result = pattern.exec(window.location.hash);
+    var postId = locationHelper.extractId();
 
-		if (result) {
-			this.$http.get('https://falai-paty-service-staging.herokuapp.com/posts/'.concat(result[0])).then(function(response) {
+		if (postId) {
+			this.$http.get('https://falai-paty-service-staging.herokuapp.com/posts/'.concat(postId)).then(function(response) {
 	      this.$set('post', response.data);
 	    });
 		} else {
